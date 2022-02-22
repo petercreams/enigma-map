ef0811b999eab174ba722d1a28a7346ab2e20b7f
Piotr Smietanka <piotrsm99@gmail.com>
2022-02-22 08:41:31 +0100

    add map styles

---
+++ src/components/Map/mapStyles.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    featureType: "landscape",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.man_made",
+    elementType: "all",
+    stylers: [
+      {
+        saturation: "-70",
+      },
+      {
+        lightness: "14",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+
+  {
+    featureType: "water",
+    elementType: "labels",
+    stylers: [
+      {
+        lightness: "12",
+      },
+    ],
+  },
+];
