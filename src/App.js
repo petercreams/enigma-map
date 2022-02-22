4c3eb69d1414d867d4b3d58b00f7305065309170
Piotr Smietanka <piotrsm99@gmail.com>
2022-02-22 08:41:16 +0100

    add app wireframe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Legend from "./components/Legend/Legend";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+
+import { CssBaseline, Grid } from "@material-ui/core";
+import { useState } from "react";
 
 function App() {
+  const [objects, setObjects] = useState([]);
   return (
     <div className="App">
-      <h1>hello</h1>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List objects={objects} setObjects={setObjects} />
+          {/* <h3>Designed & Developed</h3>
+          <h3>2022 Piotr Śmietanka</h3> */}
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map objects={objects} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
