70311fc68db9e6335cd23e77934d4e829bb4e9df
Piotr Smietanka <piotrsm99@gmail.com>
2022-02-23 11:43:15 +0100

    add simple popover

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,12 +3,13 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import styles from "./Map.module.scss";
 import mapStyles from "./mapStyles";
 
-import { Tooltip, Modal, Box, Typography } from "@mui/material";
+import { Tooltip, Modal, Box, Typography, Popover, Grid } from "@mui/material";
 import { useState, useRef } from "react";
 
 import TourIcon from "@mui/icons-material/Tour";
 import LocalParkingIcon from "@mui/icons-material/LocalParking";
 import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import DescriptionIcon from "@mui/icons-material/Description";
 import useSupercluster from "use-supercluster";
 
 import Marker from "./Marker";
@@ -16,10 +17,14 @@ import Cluster from "./Cluster";
 
 const Map = ({ objects, isClicked }) => {
   const coordinates = { lat: 52.237049, lng: 19.017532 };
-  const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const [zoom, setZoom] = useState(7);
   const [bounds, setBounds] = useState(null);
+  const [modalInfo, setModalInfo] = useState({});
   const mapRef = useRef();
+
+  const open = Boolean(anchorEl);
 
   const points = objects.map((point) => ({
     type: "Feature",
@@ -43,12 +48,21 @@ const Map = ({ objects, isClicked }) => {
 
   console.log("clusters", clusters);
 
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
+  const handleModalShow = (id, event) => {
+    // setModalInfo()
+    // setIsOpen(true);
 
-  const handleClose = () => {
-    setIsOpen(false);
+    console.log(id);
+    const modalDataArray = clusters.filter(
+      (cluster) => !cluster.properties.cluster && cluster.properties.id === id
+    );
+
+    console.log(modalDataArray[0].properties);
+    setModalInfo(modalDataArray[0].properties);
+
+    console.log(event);
+
+    // setIsModalOpen(true);
   };
 
   return (
@@ -133,73 +147,91 @@ const Map = ({ objects, isClicked }) => {
           }
 
           return (
-            <Marker
-              key={cluster.properties.id}
+            <div
               lat={latitude}
               lng={longitude}
-              name={cluster.properties.name}
-              id={cluster.properties.id}
-              discriminator={cluster.properties.discriminator}
-            />
+              onClick={(e) => {
+                console.log(e.currentTarget);
+                if (anchorEl === null) {
+                  handleModalShow(cluster.properties.id, e);
+                  setAnchorEl(e.currentTarget);
+                } else setAnchorEl(null);
+              }}
+            >
+              <Marker
+                key={cluster.properties.id}
+                lat={latitude}
+                lng={longitude}
+                name={cluster.properties.name}
+                id={cluster.properties.id}
+                discriminator={cluster.properties.discriminator}
+                // animation={anchorEl !== null}
+                clickedEl={anchorEl}
+              ></Marker>
+              {
+                <Popover
+                  anchorEl={anchorEl}
+                  open={open}
+                  anchorOrigin={{
+                    vertical: -13,
+                    horizontal: "center",
+                  }}
+                  transformOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                  }}
+                  elevation={2}
+                >
+                  <Typography
+                    sx={{
+                      p: 2,
+                      textAlign: "center",
+                      backgroundColor: "#1975d2 ",
+                      color: "white",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {modalInfo.name}
+                  </Typography>
+                  <Grid
+                    container
+                    direction="row"
+                    textAlign="center"
+                    alignItems="center"
+                  >
+                    {/* TODO: wyświetlanie informacji modalInfo?.category ? _.battery ? _.costam - zeby pasowalo do wszystkich w zaleznosci od dostepnosci */}
+                    <Grid item xs={2}>
+                      <DescriptionIcon fontSize="large" xs={2} />
+                    </Grid>
+                    <Grid item xs={10}>
+                      <Typography sx={{ p: 2 }}>
+                        {modalInfo.description}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <DescriptionIcon fontSize="large" xs={2} />
+                    </Grid>
+                    <Grid item xs={10}>
+                      <Typography sx={{ p: 2 }}>
+                        {modalInfo.description}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Popover>
+              }
+              {/* <Modal>
+                  <Box>
+                    <Typography variant="h6" component="h2">
+                      {console.log(modalInfo.name)}
+                    </Typography>
+                  </Box>
+                </Modal> */}
+            </div>
           );
         })}
-        {/* {objects?.map((place, i) => {
-          return (
-            <Marker
-              key={place.id}
-              lat={place.location.latitude}
-              lng={place.location.longitude}
-              name={place.id}
-              id={place.id}
-              discriminator={place.discriminator}
-            />
-          );
-        })} */}
       </GoogleMapReact>
 
-      {/* <div
-              className={styles.markerContainer}
-              lat={place.location.latitude}
-              lng={place.location.longitude}
-              key={i}
-            >
-              {place.discriminator === "vehicle" && (
-                <Tooltip title={place.name}>
-                  <DirectionsCarIcon
-                    key={i}
-                    lat={Number(place.location.latitude)}
-                    lng={Number(place.location.longitude)}
-                    color="primary"
-                    fontSize="large"
-                    onClick={(e) => setIsOpen(true)}
-                  />
-                </Tooltip>
-              )}
-              {place.discriminator === "parking" && (
-                <Tooltip title={place?.name}>
-                  <LocalParkingIcon
-                    key={i}
-                    lat={Number(place.location.latitude)}
-                    lng={Number(place.location.longitude)}
-                    color="primary"
-                    fontSize="large"
-                  />
-                </Tooltip>
-              )}
-
-              {place.discriminator === "poi" && (
-                <Tooltip title={place.name}>
-                  <TourIcon
-                    key={i}
-                    lat={Number(place.location.latitude)}
-                    lng={Number(place.location.longitude)}
-                    color="primary"
-                    fontSize="large"
-                  />
-                </Tooltip>
-              )}
-
-              {isOpen && (
+      {/* {isOpen && (
                 <Modal open={isOpen} onClose={handleClose} id="modal-container">
                   <Box>
                     <Typography
@@ -211,8 +243,7 @@ const Map = ({ objects, isClicked }) => {
                     </Typography>
                   </Box>
                 </Modal>
-              )}
-            </div> */}
+              )} */}
     </div>
   );
 };
